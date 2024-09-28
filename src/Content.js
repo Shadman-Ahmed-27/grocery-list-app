eeb2b657be8fb35d3e334cc10af0c70054df1c34
Shadman Ahmed <ShadmanAhmed27@gmail.com>
2024-09-27 19:36:17 -0500

    create a checkbox that can click and unclick

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
     const  [items, setItems] = useState([
@@ -18,12 +19,34 @@ const Content = () => {
             item: "Item 3"
         }
     ]);
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
+        setItems(listItems);
+    }
     
     return (
         <main>
-
+            <ul>
+                {items.map((item) => (
+                    <li class ="item" key={item.id}>
+                        <input
+                            type="checkbox"
+                            onChange={() => handleCheck(item.id)}
+                            checked={item.checked}
+                        />
+                        
+                        <label>{item.item}</label>
+                        
+                        <FaTrashAlt 
+                        role="button"
+                        tabIndex="0"
+                        />
+                    </li>
+                ))}
+            </ul>
         </main>
-  )
+    )
 }
 
 export default Content
