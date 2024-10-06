2e0af789d1e41e021e007bb8dcec53527c14dc85
Shadman Ahmed <ShadmanAhmed27@gmail.com>
2024-10-06 18:00:53 -0500

    added a search items form and styled it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
@@ -9,6 +10,7 @@ function App() {
     JSON.parse(localStorage.getItem("shoppingList"))
   );
   const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
@@ -49,6 +51,7 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
         items={items}
         handleCheck={handleCheck}
